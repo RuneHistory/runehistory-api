0dcdd92ebc52ade3e1f62aa619d83799da7c93e8
Jim Wright <jim@linx.net>
2018-02-26 08:24:46 +0000

    Implement xp_sum

--- runehistory/app/commands/highscore.py
+++ runehistory/app/commands/highscore.py
@@ -6,7 +6,7 @@ from cmdbus import Command
 from evntbus import evntbus
 from ioccontainer import inject
 
-from runehistory.app.exceptions import NotFoundError
+from runehistory.app.exceptions import RHError, NotFoundError
 from runehistory.app.services.account import AccountService
 from runehistory.app.services.highscore import HighScoreService
 from runehistory.app.events.highscore import HighScoreCreatedEvent, \
@@ -29,7 +29,25 @@ class CreateHighScoreCommand(Command):
         if not account:
             raise NotFoundError('Account not found: {}'.format(self.slug))
         highscore = self.highscore_service.create(account, self.skills)
-        evntbus.emit(HighScoreCreatedEvent(highscore))
+        evntbus.emit(HighScoreCreatedEvent(account, highscore))
+        existing = self.highscore_service.find_by_xp_sum(
+            account.id, highscore.calc_xp_sum()
+        )
+        now = datetime.utcnow()
+        new_data = {
+            'runs_unchanged': account.runs_unchanged + 1,
+            'last_run_at': now,
+        }
+        if len(existing) == 1:
+            new_data['run_changed_at'] = now
+            new_data['runs_unchanged'] = 0
+        updated = self.account_service.update_one([
+            ['id', account.id]
+        ], new_data)
+
+        if not updated:
+            raise RHError('Failed to update account')
+
         return highscore
 
 
