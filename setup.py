1f4e73770fe26ee099a0fa9137d61ae7bbcf2ba0
Jim Wright <jmwri93@gmail.com>
2018-06-06 09:05:12 +0100

    Update github url

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-github = 'https://github.com/jmwri/runehistory-api'
-version = '0.0.3'
+github = 'https://github.com/runehistory/runehistory-api'
+version = '0.0.4'
 
 setup(
     name='runehistory-api',
